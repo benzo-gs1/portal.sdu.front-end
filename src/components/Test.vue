5abcc3c0c2e9d552f12f4bf6fa1ccdaa0872eae2
RayDarar <dfqgth400@gmail.com>
2020-05-09 13:14:34 +0600

    project-rewriting-2

---
+++ src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="test">
+    Counter: {{ counter }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  public counter: number = -1;
+
+  created() {
+    this.counter = 0;
+  }
+
+  increment() {
+    this.counter++;
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
